891c18adec9e30425fca7c1e2df24ded5e4738d8
aquilas91 <agaq2020@gmail.com>
2020-10-02 08:23:20 -0500

    Completed

---
+++ db/create_product.sql
@@ -0,0 +1,3 @@
+-- INSERT INTO product ( name, description, price, image_url ) VALUES ( 'keybord', 'QUERTY', $49, https://i.ytimg.com/vi/AhbZVTaJi48/maxresdefault.jpg);
+
+INSERT INTO product ( name, description, price, image_url ) VALUES ( $1, $2, $3, $4 );
